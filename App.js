068c06aa8198d90afccc91822a02596548be4679
kaw <kaw@aetherview.io>
2023-05-28 11:11:03 -0300

    build3

--- App.js
+++ App.js
@@ -2,20 +2,26 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import * as React from 'react-native'
 import { WebView } from 'react-native-webview'
+import swal from 'sweetalert2'
+
+const runFirst = `alert('AetherView é apenas um webview para o chat.openai.com, o projeto ainda se encontra na sua versão Alpha.')`;
 
 export default function App() {
-  return (
-    <WebView
-      style = {styles.container}
-      source={{ uri: 'https://chat.openai.com'}}
-    />
-  );
-}
+    return(
+      <WebView
+        style={styles.container}
+        source={{ uri: 'https://chat.openai.com/' }}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        startInLoadingState={true}
+        injectedJavaScriptBeforeContentLoaded={runFirst}
+      />
+    );
+ }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
